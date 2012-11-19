8ffa8a9062ae895f697ef507866297950467fcf3
dwhughes <dwhughes@DM100090.roche.com.au>
2012-11-19 10:49:18 +1000

    .

--- dystill/index.php
+++ dystill/index.php
@@ -1,8 +1,12 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
-<title>Insert title here</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+	<title>Dystopian Illusion</title>
+	<meta http-equiv="Content-Language" content="en-us" />
+	
+	<meta http-equiv="imagetoolbar" content="no" />
+	
 </head>
 <frameset>
     <frame>
@@ -11,7 +15,7 @@
     <body>
     <p>This pagettes uses frames.Test The current browser you are using does not support frames.</p>
     <?php
-
+		
 	?>
     </body>
     </noframes>
