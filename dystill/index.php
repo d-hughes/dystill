48f4ccccc553df947d0f3119033ba369a6345905
dwhughes <dwhughes@DM100090.roche.com.au>
2012-11-20 12:23:56 +1000

    .

--- dystill/index.php
+++ dystill/index.php
@@ -23,7 +23,12 @@
     	
     	<div id="header">Header</div>
     	
-    	<div id="sidebar-a">Sidebar A</div>
+    	<div id="sidebar-a">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
+	Praesent sapien purus, ultrices a, varius ac, suscipit ut, enim. Maecenas in lectus. 
+	Donec in sapien in nibh rutrum gravida. Sed ut mauris. Fusce malesuada enim vitae lacus 
+	euismod vulputate. Nullam rhoncus mauris ac metus. Maecenas vulputate aliquam odio. 
+	Duis scelerisque justo a pede. Nam augue lorem, semper at, porta eget, placerat eget, 
+	purus. Suspendisse mattis nunc vestibulum ligula. In hac habitasse platea dictumst.</div>
     	
     	<div id="content">
 	Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
