b91a5a532af9c1e6e40e80aa5414b83b00add8a2
Dwayne Hughes <nitemonq@gmail.com>
2012-12-03 22:05:20 +1000

    .

--- dystill/index.php
+++ dystill/index.php
@@ -19,9 +19,11 @@
     <body>
     
     <div id="page-container">
+    
     <div id="side-container">
     <p>Testing</p>
     </div>
+    
     <div id="main-container">
     	
     	<div id="header">
@@ -31,12 +33,7 @@
     	
     	<div id="main-nav">Main Navigation</div>
     	
-    	<div id="sidebar-a">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
-	Praesent sapien purus, ultrices a, varius ac, suscipit ut, enim. Maecenas in lectus. 
-	Donec in sapien in nibh rutrum gravida. Sed ut mauris. Fusce malesuada enim vitae lacus 
-	euismod vulputate. Nullam rhoncus mauris ac metus. Maecenas vulputate aliquam odio. 
-	Duis scelerisque justo a pede. Nam augue lorem, semper at, porta eget, placerat eget, 
-	purus. Suspendisse mattis nunc vestibulum ligula. In hac habitasse platea dictumst.</div>
+    	<div id="sidebar-a">SIDE BAR</div>
     	
     	<div id="content">
 	Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
